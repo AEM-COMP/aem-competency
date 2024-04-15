0df9eee46139be12c47653538dc392713d18a9eb
mayukhs2904 <mayukhs@adobe.com>
2024-04-15 18:42:44 +0530

    footer code updated

--- blocks/footer/footer.js
+++ blocks/footer/footer.js
@@ -17,5 +17,19 @@ export default async function decorate(block) {
   const footer = document.createElement('div');
   while (fragment.firstElementChild) footer.append(fragment.firstElementChild);
 
+  const ul= footer.querySelectorAll('.default-content-wrapper>ul');
+  const container= document.createElement('div');
+  const containerLine = document.createElement('div');
+  const containerElement = document.createElement('div');
+  container.classList.add('footerLink');
+  containerElement.classList.add('footerElement');
+  container.append(containerLine);
+  container.append(containerElement);
+  
+  containerLine.classList.add('footerLine');
+
+  containerElement.append(ul[1]);
+  containerElement.append(ul[2]);
+  footer.querySelector('.default-content-wrapper').append(container);
   block.append(footer);
 }
